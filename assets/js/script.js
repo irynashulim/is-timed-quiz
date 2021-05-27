e756fbe2cdbfefbee748d8706bf5934d5ff56fa1
Iryna <iryna.shulim@gmail.com>
2021-05-26 22:52:35 -0500

    update and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelector(".choice-text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
 
