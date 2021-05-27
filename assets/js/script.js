6f3b2fde1c223b7ff8181b9061491b26100e06a6
Iryna <iryna.shulim@gmail.com>
2021-05-26 23:52:12 -0500

    result box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,8 @@
-const question = document.querySelector("#question");
-const choices = Array.from(document.querySelector(".choice-text"));
-const progressText = document.querySelector("#progressText");
-const scoreText = document.querySelector("#score");
+//selecting all required elements
+const startBtn = document.querySelector("#start-btn");
+const quizContainer = document.querySelector(".quiz-container");
+const resultContainer = document.querySelector(".result-container");
 
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
 
 
