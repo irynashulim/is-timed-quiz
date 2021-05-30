20c2fb7a93f5b4fc1de6c71f9fcc0cfd600f44ff
Iryna <iryna.shulim@gmail.com>
2021-05-29 22:59:51 -0500

    functions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,7 +5,7 @@ let questions = [
         choice2: "msgBox('Hello World');",
         choice3: "alertBox('Hello World');",
         choice4: "alert('Hello World');",
-        answer: 3,
+        correctAnswer: 3,
     },
     {
         question: "How to empty an array in JavaScript?",
@@ -13,7 +13,7 @@ let questions = [
         choice2: "arrayList(0)",
         choice3: "arrayList.length=0",
         choice4: "arrayList.len(0)",
-        answer: 2,
+        correctAnswer: 2,
     },
     {
         question: "What function to add an element at the begining of an array and one at the end?",
@@ -21,7 +21,7 @@ let questions = [
         choice2: "unshift,push",
         choice3: "first,push",
         choice4: "unshift,last",
-        answer: 1,  
+        correctAnswer: 1,  
     },
     {
         question: "What will this output? var a = [1, 2, 3]; console.log(a[6]);",
@@ -29,7 +29,7 @@ let questions = [
         choice2: "0",
         choice3: "prints nothing",
         choice4: "Syntax error",
-        answer: 0,  
+        correctAnswer: 0,  
     },
     {
         question: "What would following code return? console.log(typeof typeof 1);",
@@ -37,7 +37,7 @@ let questions = [
         choice2: "number",
         choice3: "Syntax error",
         choice4: "undefined",
-        answer: 0,  
+        correctAnswer: 0,  
     },
     {
         question: "Which software company developed JavaScript?",
@@ -45,7 +45,7 @@ let questions = [
         choice2: "Netscape",
         choice3: "Sun Microsystems",
         choice4: "Oracle",
-        answer: 1,      
+        acorrectAnswer: 1,      
     },
     {
         question: "What would be the result of 3+2+'7'?",
@@ -53,7 +53,7 @@ let questions = [
         choice2: "12",
         choice3: "14",
         choice4: "57",
-        answer: 3,      
+        correctAnswer: 3,      
     },
     {
         question: "Look at the following selector: $('div'). What does it select?",
@@ -61,7 +61,7 @@ let questions = [
         choice2: "The last div element",
         choice3: "All div elements",
         choice4: "Current div element",
-        answer: 2,  
+        correctAnswer: 2,  
     },
     {
         question: "How can a value be appended to an array?",
@@ -69,7 +69,7 @@ let questions = [
         choice2: " arr[arr.length]=value;",
         choice3: "arr[]=add(value);",
         choice4: "None of these",
-        answer: 1,  
+        correctAnswer: 1,  
     },
     {
         question: "What will the code below output to the console? console.log(1 + +'2' + '2');",
@@ -77,6 +77,6 @@ let questions = [
         choice2: "'122'",
         choice3: "'13'",
         choice4: "'14'",
-        answer: 0,  
+        correctAnswer: 0,  
     }
 ]
