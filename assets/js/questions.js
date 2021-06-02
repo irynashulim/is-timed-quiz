4f84a082c18ee81c9f8d2c741f1f1b2009cefb65
Iryna <iryna.shulim@gmail.com>
2021-06-02 00:04:59 -0500

    update

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,91 +2,91 @@ let questions = [
     {
         question: "How do you write 'Hello World' in an alert box?",
         answers:
-            [" msg('Hello World')",
-                "msgBox('Hello World');",
-                "alertBox('Hello World');",
-                "alert('Hello World');"],
-        correctAnswer: 3,
+            ["1. msg('Hello World')",
+                "2. msgBox('Hello World');",
+                "3. alertBox('Hello World');",
+                "4. alert('Hello World');"],
+        correctAnswer: "3"
     },
     {
         question: "How to empty an array in JavaScript?",
         answers:
-               [ "arrayList[]",
-                "arrayList(0)",
-                "arrayList.length=0",
-                "arrayList.len(0)"],
-        correctAnswer: 2,
+            ["1. arrayList[]",
+                "2. arrayList(0)",
+                "3. arrayList.length=0",
+                "4. arrayList.len(0)"],
+        correctAnswer: "2"
     },
     {
         question: "What function to add an element at the begining of an array and one at the end?",
         answers:
-            ["push,unshift",
-                "unshift,push",
-                "first,push",
-                "unshift,last"],
-        correctAnswer: 1,
+            ["1. push,unshift",
+                "2. unshift,push",
+                "3. first,push",
+                "4. unshift,last"],
+        correctAnswer: "1"
     },
     {
         question: "What will this output? var a = [1, 2, 3]; console.log(a[6]);",
         answers:
-            ["undefined",
-                "0",
-                "prints nothing",
-                "Syntax error"],
-        correctAnswer: 0,
+            ["1. undefined",
+                "2. 0",
+                "3. prints nothing",
+                "4. Syntax error"],
+        correctAnswer: "0"
     },
     {
         question: "What would following code return? console.log(typeof typeof 1);",
         answers:
-            ["string",
-                "number",
-                "Syntax error",
-                "undefined"],
-        correctAnswer: 0,
+            ["1. string",
+                "2. number",
+                "3. Syntax error",
+                "4. undefined"],
+        correctAnswer: "0"
     },
     {
         question: "Which software company developed JavaScript?",
         answers:
-            ["Mozilla",
-                "Netscape",
-                "Sun Microsystems",
-                "Oracle"],
-        acorrectAnswer: 1,
+            ["1. Mozilla",
+                "2. Netscape",
+                "3. Sun Microsystems",
+                "4. Oracle"],
+        acorrectAnswer: "1"
     },
     {
         question: "What would be the result of 3+2+'7'?",
         answers:
-            ["327",
-                "12",
-                "14",
-                "57"],
-        correctAnswer: 3,
+            ["1. 327",
+                "2. 12",
+                "3. 14",
+                "4. 57"],
+        correctAnswer: "3"
     },
     {
         question: "Look at the following selector: $('div'). What does it select?",
         answers:
-            ["The first div element",
-                "The last div element",
-                "All div elements",
-                "Current div element"],
-        correctAnswer: 2,
+            ["1. The first div element",
+                "2. The last div element",
+                "3. All div elements",
+                "4. Current div element"],
+        correctAnswer: "2"
     },
     {
         question: "How can a value be appended to an array?",
         answers:
-            ["arr(length).value;",
-                " arr[arr.length]=value;",
-                "arr[]=add(value);",
-                "None of these"],
-        correctAnswer: 1,
+            ["1. arr(length).value;",
+                "2. arr[arr.length]=value;",
+                "3. arr[]=add(value);",
+                "4. None of these"],
+        correctAnswer: "1"
     },
     {
         question: "What will the code below output to the console? console.log(1 + +'2' + '2');",
         answers:
-            ["'32'",
-                "'122'",
-                "'13'",
-                "'14'"],
-        correctAnswer: 0,
+            ["1. '32'",
+                "2. '122'",
+                "3. '13'",
+                "4. '14'"],
+        correctAnswer: "0"
     }
 ]
